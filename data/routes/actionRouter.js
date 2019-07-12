72d5b50577ffe5b8309e0bd408835d136bba33c1
Anna W <anski.anna@gmail.com>
2019-07-12 08:53:24 +0100

    post new action working

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -17,11 +17,57 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
     try {
         const action = await actionDb.get(id)
-        res.status(200).json(action);  
+        if(action){
+            res.status(200).json(action);  
+        } else{
+            res.status(404).json({ message: 'action with that id not found'})
+        }
     } catch (error){
-        res.status(500).json({ message: 'error getting post with that id'})
+        res.status(500).json({ message: 'error getting action with that id'})
     }
 })
 
+router.post('/', async (req, res) => {
+    const {project_id, description, notes, completed} = req.body;
+    if (!project_id) {
+        res.status(400).json({error: `Need to Provide Project Id`})
+    } else if (!description.substr(1,128)) {
+        res.status(400).json({error: `Need to Provide Description Less than 128 Characters`})
+    } else if (!notes) {
+        res.status(400).json({error: `Need to Provide Notes`})
+    } else {
+        actionDb
+        .insert({project_id, description, notes, completed})
+        .then(newAction => {
+            res.status(200).json(newAction)
+        })
+        .catch(() => {
+            res.status(500).json({error: `Could Not Add New Action`})
+        })
+    }
+    // const actionData = req.body;
+    // const {project_id, description, notes, completed} = req.body;
+    // try {
+    //     const action = await actionDb.insert({ project_id, description, notes, completed });
+    //     if (!project_id) {
+    //         res.status(400).json({error: `Need to Provide Project Id`})
+    //     } else if (!description) {
+    //         res.status(400).json({error: `Need to Provide Description Less than 128 Characters`})
+    //     } else if (!notes) {
+    //         res.status(400).json({error: `Need to Provide Notes`})
+    //     } else {
+    //         res.status(201).json(actionData)
+    //     }
+        // if(!actionData){
+        //     res.status(400).json({ message: 'missing body of request'})
+        // } else {
+        //     res.status(201).json(actionData)
+        // }
+    // } catch (error) {
+    //     res.status(500).json({
+    //         message: 'Error adding the new action',
+    //       });
+    // }
+}) 
 
 module.exports = router;
